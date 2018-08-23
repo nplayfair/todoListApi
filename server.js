0f76c19c81a41b61d99a7d21095283a9ebe98054
Nick Playfair <nick@chaosemerald.co.uk>
2018-08-23 13:33:51 +0100

    configure server

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 var express = require('express'),
         app = express(),
-      port  = process.env.PORT || 3000;
+      port  = process.env.PORT || 3000,
+  mongoose  = require('mongoose'),
+      Task  = require('./api/models/todoListModel'),
+bodyParser  = require('body-parser');
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// import routes
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
 
 app.listen(port);
 
